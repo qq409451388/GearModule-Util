9524b04eec6cf570141a5fa8fa139a1b103d0163
guohan <poethan@foxmail.com>
2024-09-25 19:02:26 +0800

    feat: Guide.md added

---
+++ validator/Validator.php
@@ -0,0 +1,37 @@
+<?php
+class Validator extends Anno {
+    public $fields = [];
+
+    /**
+     * 指定注解可以放置的位置（默认: 所有）@see AnnoElementType
+     */
+    public static function constTarget()
+    {
+        return AnnoElementType::TYPE_METHOD;
+    }
+
+    /**
+     * 指定注解的执行模式 @see AnnoPolicyEnum
+     */
+    public static function constPolicy()
+    {
+        return AnnoPolicyEnum::POLICY_RUNTIME;
+    }
+
+    /**
+     * 指定注解的value设置规则 @see AnnoValueTypeEnum
+     */
+    public static function constStruct()
+    {
+        return AnnoValueTypeEnum::TYPE_RELATION;
+    }
+
+    /**
+     * 非必须，切面逻辑类名，触发此注解时，执行的逻辑
+     * @example {@see DiAspect}
+     */
+    public static function constAspect()
+    {
+        return ValidatorAspect::class;
+    }
+}
---
+++ validator/ValidatorAspect.php
@@ -0,0 +1,4 @@
+<?php
+class ValidatorAspect extends Aspect {
+
+}
